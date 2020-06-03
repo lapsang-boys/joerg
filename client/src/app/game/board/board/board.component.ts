3ff516b54e7bc2d9f1fe0b1c074b60734325b4ce
Henry Eklind <henry.eklind@tink.se>
2020-06-04 00:15:22 +0200

    feat(choice): Add ghetto choice maker

--- client/src/app/game/board/board/board.component.ts
+++ client/src/app/game/board/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../game.service';
+import { Choice } from 'src/app/game/types/choice.type';
 
 @Component({
 	selector: 'app-board',
@@ -9,6 +10,7 @@ import { GameService } from '../../game.service';
 export class BoardComponent implements OnInit {
 
 	poleIndex: number;
+	choice: Choice;
 
 	constructor(private game: GameService) { }
 
@@ -16,10 +18,16 @@ export class BoardComponent implements OnInit {
 		this.game.getObservableBoard().subscribe(board => {
 			this.poleIndex = board.pole.index;
 		});
+		this.game.getObservableChoice().subscribe(choice => {
+			this.choice = choice;
+		});
 	}
 
 	next() {
 		this.game.next();
 	}
 
+	choose(choiceIndex: number): void {
+		this.game.makeChoice(choiceIndex);
+	}
 }
